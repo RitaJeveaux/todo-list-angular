f726b2418156005a1dd701d42ed6c65740cbe79d
RitaJeveaux <ritajeveaux@gmail.com>
2025-10-22 15:26:27 -0300

    task-list criado

--- src/app/components/task-item/task-item.ts
+++ src/app/components/task-item/task-item.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITask, TaskService } from '../../services/task-service';
+
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [TaskService],
   templateUrl: './task-item.html',
   styleUrl: './task-item.css'
 })
 export class TaskItem {
-
+  @Input() task: ITask | null = null;
 }
